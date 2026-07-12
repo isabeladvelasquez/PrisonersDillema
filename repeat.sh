80babab8132c93b4cdd2d077e7dea7915a95dc3a
Isabela Dias Velasquez <isabela.velasquez05@gmail.com>
2026-07-12 20:53:37 -0300

    Update repeat.sh
    updated the repeat.sh

--- repeat.sh
+++ repeat.sh
@@ -1,10 +1,20 @@
-reps=25
-folderOutput="YOUR_FOLDER"
+#!/bin/bash
 
-gcc -o 'YOUR_CODE' 'YOUR_CODE.c' -O3 -lgsl -lgslcblas -lm
+reps=100
+folder1="SPD_2C_L100_T10000_TIMEvsPCP"
+folder2="SPD_2C_L100_T10000_2CPROB"
+folder3="SPD_2C_L100_T10000_SNAPSHOT"
+
+gcc '2C_L100_RHOvar_Mvar.c' lat2eps_lib.c -o '2C_L100_RHOvar_Mvar' -O3 -lgsl -lgslcblas -lm
 
 for i in $(seq 1 $reps); do
-    ./YOUR_CODE
-    fileOutput=$(ls -t1 | head -n1)
-    mv "$fileOutput" "$folderOutput/"
+    ./2C_L100_RHOvar_Mvar 
 done
+
+#mkdir -p "$folder1"
+#mkdir -p "$folder2"
+#mkdir -p "$folder3"
+
+#mv *SAVETIME*.dat "$folder1/"/ 2>/dev/null
+#mv *SAVE2CPROB*.dat "$folder2/"/ 2>/dev/null
+#mv *ATVS*.eps "$folder3/"/ 2>/dev/null
